e3d14e13351d73ab0daf15fe85dea25316ed97a7
Carter Rabasa <carter.rabasa@gmail.com>
2014-10-13 08:17:52 -0700

    freeze for Dreamforce

--- index.rb
+++ index.rb
@@ -1,32 +1,32 @@
 require 'sinatra'
 require 'twilio-ruby'
 
-get '/sms' do
-  response = Twilio::TwiML::Response.new do |r|
+get '/message' do 
+  twiml = Twilio::TwiML::Response.new do |r|
     r.Message do |m|
-      m.Body "What happens you put the power of the telecommunications industry in the hands of web developers?"
-      m.Media 'http://linode.rabasa.com/mindblown.gif'
+      m.Body "What happens when you put the power of telecommunications in the hands of developers?"
+      m.Media "/img/mindblown.gif"
     end
   end
   content_type 'text/xml'
-  response.text
-end
-
-get '/token' do
-  capability = Twilio::Util::Capability.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
-  capability.allow_client_outgoing 'APc11a96ef976afd91cd58ca11a9daf118'
-  capability.generate
+  twiml.text
 end
 
 get '/random' do
   client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
-  nums = client.messages.list(to:'+13059648570').collect{|m| m.from}
-  random = '+12022856865' # nums.sample
-  response = Twilio::TwiML::Response.new do |r|
-    r.Dial callerId: '+13059648570' do |d|
-      d.Number random
+  nums = client.messages.list(to: '+12027598424').collect{|m| m.from}.uniq
+  num = nums.sample
+  twiml = Twilio::TwiML::Response.new do |r|
+    r.Dial callerId: '+12027598424' do |d|
+      d.Number num
     end
   end
   content_type 'text/xml'
-  response.text  
+  twiml.text  
+end
+
+get '/token' do
+  capability = Twilio::Util::Capability.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+  capability.allow_client_outgoing 'APc8bd72e65533ce7dc6f90856396bdf50'
+  capability.generate
 end
